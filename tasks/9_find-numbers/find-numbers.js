00d9e90667fceed6a71c58aee08acc7df0f386f7
Mikhail Ilichev <miffn3@yandex.ru>
2019-09-10 01:06:42 +0400

    chore: task 9

---
+++ tasks/9_find-numbers/find-numbers.js
@@ -0,0 +1,10 @@
+export function getNumbers(str) {
+  let regex = /\d+/g;
+  let stringDigits = str.match(regex);
+  let digits = [];
+  
+  for (let i = 0; i < stringDigits.length; i++) {
+    digits.push(+stringDigits[i]);
+  }
+  return digits;
+}
