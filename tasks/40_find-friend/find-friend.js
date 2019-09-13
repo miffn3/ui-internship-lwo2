40bc91a1f29e301852661dc4b9cc292c89e6a1d2
Mikhail <miffn3@yandex.ru>
2019-09-13 18:17:51 +0400

    fix: task 40

--- tasks/40_find-friend/find-friend.js
+++ tasks/40_find-friend/find-friend.js
@@ -1,24 +1,13 @@
+const BLUE = 'blue';
+
 export function findFriend(arr) {
   let friends = 0;
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === 'red') {
-      if (i > 1) {
-        if (arr[i - 2] === 'blue' && arr[i - 1] === 'blue') {
-          friends++;
-          continue;
-        }
-      }
-      if (i > 0 && i < arr.length) {
-        if (arr[i - 1] === 'blue' && arr[i + 1] === 'blue') {
-          friends++;
-          continue;
-        }
-      }
-      if (i < arr.length - 1) {
-        if (arr[i + 1] === 'blue' && arr[i + 2] === 'blue') {
-          friends++;
-          continue;
-        }
+      if (arr[i - 2] === BLUE && arr[i - 1] === BLUE ||
+          arr[i - 1] === BLUE && arr[i + 1] === BLUE ||
+          arr[i + 1] === BLUE && arr[i + 2] === BLUE) {
+        friends++;
       }
     }
   }
