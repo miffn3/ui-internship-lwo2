e687b7527caff39ed3fcf25613119f6c7495793b
Mikhail <miffn3@yandex.ru>
2019-09-14 23:04:06 +0400

    feature: task 42

---
+++ tasks/42_first-n-smallest/first-n-smallest.js
@@ -0,0 +1,23 @@
+export function firstNSmallest(arr, n) {
+  let count = 0;
+  let lastMin = arr.reduce((last) => {
+    if (count >= n) {
+      return last;
+    }
+    let min = arr.reduce((acc, cur) => {
+      if (cur < acc && cur > last) {
+        return cur;
+      }
+      return acc;
+    }, Number.MAX_SAFE_INTEGER);
+    count += arr.reduce((acc, cur) => {
+      if (cur === min) {
+        return ++acc;
+      }
+      return acc;
+    }, 0);
+    return min;
+  }, Number.MIN_SAFE_INTEGER);
+  let newLength = 0;
+  return arr.filter((item) => item <= lastMin && ++newLength <= n);
+}
