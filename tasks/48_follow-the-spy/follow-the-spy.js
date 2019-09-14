4338c8c27d51e10618d73004770520f950988762
Mikhail <miffn3@yandex.ru>
2019-09-14 23:41:56 +0400

    feature: task 48

---
+++ tasks/48_follow-the-spy/follow-the-spy.js
@@ -0,0 +1,21 @@
+const sortCoutries = (a, b) => {
+  if (a[1] === b[0]) {
+    return -1;
+  }
+  if (a[0] === b[1]) {
+    return 1;
+  }
+  return 0;
+};
+
+export function followTheSpy(routes) {
+  return routes.sort(sortCoutries).reduce((acc, cur, index) => {
+    if (index === 0) {
+      return acc.concat(cur[0] + ', ' + cur[1] + ', ');
+    }
+    if (index === routes.length - 1) {
+      return acc.concat(cur[1]);
+    }
+    return acc.concat(cur[1] + ', ');
+  }, '');
+}
