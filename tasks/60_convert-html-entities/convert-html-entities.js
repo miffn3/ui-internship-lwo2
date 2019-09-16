06dfc9a501c075a6ef8a6d07d7566834f539d0a3
Mikhail <miffn3@yandex.ru>
2019-09-16 19:56:46 +0400

    feature: task 60

---
+++ tasks/60_convert-html-entities/convert-html-entities.js
@@ -0,0 +1,7 @@
+export function convertHTML(str) {
+  return str.replace(/&/g,'&amp;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;')
+      .replace(/"/g, '&quot;')
+      .replace(/'/g, '&apos;');
+}
