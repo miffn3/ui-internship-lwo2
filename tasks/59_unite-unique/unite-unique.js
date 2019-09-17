c0df68dd0cd7e17d105fb917f7038a6089475e10
Mikhail <miffn3@yandex.ru>
2019-09-17 14:12:13 +0400

    feature: task 59

---
+++ tasks/59_unite-unique/unite-unique.js
@@ -0,0 +1,11 @@
+export function uniteUnique(...arrays) {
+  const result = [];
+  for (let i = 0; i < arrays.length; i++) {
+    for (let j = 0; j < arrays[i].length; j++) {
+      if (!result.includes(arrays[i][j])) {
+        result.push(arrays[i][j]);
+      }
+    }
+  }
+  return result;
+}
