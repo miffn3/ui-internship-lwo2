471f2eb84c7ee79f5b9d207ff5d976b700bc9736
Mikhail <miffn3@yandex.ru>
2019-09-25 22:48:37 +0400

    fix: task 48

--- tasks/48_follow-the-spy/follow-the-spy.js
+++ tasks/48_follow-the-spy/follow-the-spy.js
@@ -1,22 +1,22 @@
-const sortCountries = (a, b) => {
-  if (a[1] === b[0]) {
-    return -1;
-  }
-  if (a[0] === b[1]) {
-    return 1;
-  }
-  return 0;
-};
-
 export function followTheSpy(routes) {
-  const countries = routes.sort(sortCountries);
+  const countries = routes.slice();
+  let currentPair = countries[0];
+  let indexOfCurrentPair = 0;
+  for (let i = 1; i < countries.length; i++) {
+    if (currentPair[1] === countries[i][0]) {
+      const tmp = countries[indexOfCurrentPair + 1];
+      countries[indexOfCurrentPair + 1] = countries[i];
+      countries[i] = tmp;
+      indexOfCurrentPair++;
+    }
+  }
   return countries.reduce((acc, cur, index) => {
     if (index === 0) {
       return acc + cur[0] + ', ' + cur[1] + ', ';
     }
     if (index === routes.length - 1) {
-      return acc+ cur[1];
+      return acc + cur[1];
     }
-    return acc+ cur[1] + ', ';
+    return acc + cur[1] + ', ';
   }, '');
 }
