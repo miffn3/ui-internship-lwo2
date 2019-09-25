eefcf160d0d7a7e8cb47d5482e4b8828f87524e4
Mikhail <miffn3@yandex.ru>
2019-09-25 13:35:44 +0400

    style: task 91

--- tasks/91_make-a-person/make-a-person.js
+++ tasks/91_make-a-person/make-a-person.js
@@ -5,21 +5,27 @@
  */
 export function Person(fullName) {
   let splittedName = fullName.split(' ');
+
   this.getFirstName = function() {
     return splittedName[0];
   };
+
   this.getLastName = function() {
     return splittedName[1];
   };
+
   this.getFullName = function() {
     return splittedName[0] + ' ' + splittedName[1];
   };
+
   this.setFirstName = function(firstNameNew) {
     splittedName[0] = firstNameNew;
   };
+
   this.setLastName = function(lastNameNew) {
     splittedName[1] = lastNameNew;
   };
+
   this.setFullName = function(fullName) {
     let name = fullName.split(' ');
     splittedName[0] = name[0];
